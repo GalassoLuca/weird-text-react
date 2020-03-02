8ad883102bb1242164c216d8649e77ff774d4947
Luca Galasso <galasso.luca@outlook.com>
2020-03-02 23:09:45 +0100

    fix: encode one long word

--- src/Helpers/encode.js
+++ src/Helpers/encode.js
@@ -1,8 +1,15 @@
 export function encode ({ text = '' } = {}) {
   if (!text.length) return {}
 
+  let encodedText = text
+
+  if (text.length > 3) {
+    const [a, b, c] = text
+    encodedText = text[0] + c + b + text.substring(3)
+  }
+
   return {
     words: [text],
-    encodedText: text
+    encodedText
   }
 }
--- src/Helpers/encode.test.js
+++ src/Helpers/encode.test.js
@@ -15,3 +15,15 @@ test('should return the expected for one short word', () => {
 
   expect(output).toEqual(expected)
 })
+
+test('should return the expected for one long word', () => {
+  const text = 'Hello'
+  const expected = {
+    words: ['Hello'],
+    encodedText: 'Hlelo'
+  }
+
+  const output = encode({ text })
+
+  expect(output).toEqual(expected)
+})
