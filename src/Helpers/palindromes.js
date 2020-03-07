13b92d712f5cc2ad0a5f039569fd15a33208aab5
Luca Galasso <galasso.luca@outlook.com>
2020-03-07 09:48:05 +0100

    fix(palindromes): remove palindromes check

--- src/Helpers/palindromes.js
+++
@@ -1,6 +0,0 @@
-export default function isPalindromes (text = '', { normalize = w => w.toLowerCase() } = {}) {
-  const reverse = text.split("").reverse().join('')
-
-  console.log(normalize(text) + ' vs ' + normalize(reverse))
-  return normalize(text) === normalize(reverse)
-}
