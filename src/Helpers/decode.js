85d242529079303214420253ca036f5c5c40e1d3
Luca Galasso <galasso.luca@outlook.com>
2020-03-09 00:01:26 +0100

    fix(decode): decode single and multiple words

--- src/Helpers/decode.js
+++ src/Helpers/decode.js
@@ -1,7 +1,39 @@
-export default function decode (text = '', words = []) {
-  if (words.length === 0) {
-    return { warn: 'No words specified' }
+import getWords from './getWords'
+
+export default function decode(text = '', originalWords = []) {
+  if (originalWords.length === 0) {
+    return { warnings: { missingWords: true } }
   }
 
-  return text
+  let decodedText = text
+  const { words: wordsToDecode } = getWords(text)
+
+  const originalWordsHasMap = originalWords.reduce(toMap, new Map())
+  const wordsToDecodeHasMap = wordsToDecode.reduce(toMap, new Map())
+
+  wordsToDecode.forEach(wordToDecode => {
+    const key = getKey(wordToDecode)
+    const values = originalWordsHasMap.get(key)
+
+    if (values.length > 1) throw new Error('handle this case')
+
+    decodedText = decodedText.replace(wordToDecode, values[0])
+  })
+
+  return { decodedText }
+}
+
+function getKey(string) {
+  return string.split('').sort().join('')
+}
+
+function toMap(map, word) {
+  const key = getKey(word)
+  const words = map.get(key) || []
+
+  words.push(word)
+
+  map.set(key, words)
+
+  return map
 }
--- src/Tests/Helpers/decode.test.js
+++ src/Tests/Helpers/decode.test.js
@@ -5,7 +5,29 @@ test('should return a warning if the words are missing', () => {
 
   const result = decode(encrypted)
 
-  expect(result.warn).toBeDefined()
+  expect(result.warnings.missingWords).toBeTruthy()
+})
+
+test('should decrypt a single word', () => {
+  const original = 'word'
+
+  const words = [original]
+  const encrypted = 'wrod'
+
+  const result = decode(encrypted, words)
+
+  expect(result.decodedText).toEqual(original)
+})
+
+test('should decrypt a multiple words', () => {
+  const original = 'Hello World from Luca'
+
+  const words = ['Hello', 'World', 'from', 'Luca']
+  const encrypted = 'Hlelo Wrold form Lcua'
+
+  const result = decode(encrypted, words)
+
+  expect(result.decodedText).toEqual(original)
 })
 
 test.skip('should return a warning if a word can be decoded to multiple words', () => {
